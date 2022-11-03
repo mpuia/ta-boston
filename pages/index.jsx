bc7277f5454695d840c5098f9e1c94501978931f
mpuia <110475140+mpuia@users.noreply.github.com>
2022-11-03 22:02:07 +0200

    ta

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return <div>
-    hello boston <i className="fa-solid fa-house"></i>
+      hello boston <i className="fa-solid fa-house"></i>
 
-  <img src="/images/logo.jpg" alt="Alt"></img>
-  </div>
+      <img src="/images/logo.jpg" alt="Alt"></img>
+
+      <button className="button button-primary">Button Primary</button>
+      <button className="button button-primary button-large">Button Primary Large</button>
+      <a className="button button-primary">Button Primary</a>
+      <a className="button button-primary button-large">Button Primary Large</a>
+    </div>
 }
