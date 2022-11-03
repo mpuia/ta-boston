6a3d425b07babe3fb4de6c9ce74e879d6bb5b854
mpuia <110475140+mpuia@users.noreply.github.com>
2022-11-03 19:58:07 +0200

    build

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>hello boston <i className="fa-solid fa-house"></i>
+  return <div>
+    hello boston <i className="fa-solid fa-house"></i>
+
+  <img src="/images/logo.jpg" alt="Alt"></img>
   </div>
 }
